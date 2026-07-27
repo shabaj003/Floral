e137d164c9542a7f7f6c438859d9fc4e4907ab53
shabaj003 <mulanishahabaj90@gmail.com>
2026-07-27 23:22:21 +0530

    Mobile Fix

--- .vercel/output/functions/__server.func/_ssr/router-6DL7uEb_.mjs
+++ .vercel/output/functions/__server.func/_ssr/router-6DL7uEb_.mjs
@@ -2,10 +2,10 @@ import { o as __toESM } from "../_runtime.mjs";
 import { n as require_jsx_runtime, r as require_react, t as QueryClientProvider } from "../_libs/react+tanstack__react-query.mjs";
 import { c as HeadContent, d as Outlet, f as lazyRouteComponent, g as useRouter, h as Link, m as createRootRouteWithContext, p as createFileRoute, s as Scripts, u as createRouter } from "../_libs/@tanstack/react-router+[...].mjs";
 import { t as QueryClient } from "../_libs/tanstack__query-core.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/router-D7b9iITY.js
+//#region node_modules/.nitro/vite/services/ssr/assets/router-6DL7uEb_.js
 var import_react = /* @__PURE__ */ __toESM(require_react());
 var import_jsx_runtime = require_jsx_runtime();
-var styles_default = "/assets/styles-Idg4X4di.css";
+var styles_default = "/assets/styles-BtxAosJI.css";
 function reportLovableError(error, context = {}) {
 	if (typeof window === "undefined") return;
 	window.__lovableEvents?.captureException?.(error, {
--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-0WY6smiG.mjs
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-0WY6smiG.mjs
@@ -1,19 +1,19 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DVagKcKT.js
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-0WY6smiG.js
 var tsrStartManifest = () => ({ routes: {
 	__root__: {
 		filePath: "C:/Users/Dell/Desktop/Floral/src/routes/__root.tsx",
 		children: ["/"],
-		preloads: ["/assets/index-CGL30789.js"],
+		preloads: ["/assets/index-SVTQRdoE.js"],
 		scripts: [{ attrs: {
 			type: "module",
 			async: !0,
-			src: "/assets/index-CGL30789.js"
+			src: "/assets/index-SVTQRdoE.js"
 		} }]
 	},
 	"/": {
 		filePath: "C:/Users/Dell/Desktop/Floral/src/routes/index.tsx",
 		children: void 0,
-		preloads: ["/assets/routes-BXcpOHmO.js"]
+		preloads: ["/assets/routes-Dm08FmN1.js"]
 	}
 } });
 //#endregion
